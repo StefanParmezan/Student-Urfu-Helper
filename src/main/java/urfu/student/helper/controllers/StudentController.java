cdb7e8b690e8d7cb08acf25c7eb344477024a378
daniil4jk <daniilprotop@bk.ru>
2025-11-22 02:57:39 +0500

    исправил контроллер студента

--- src/main/java/urfu/student/helper/controllers/StudentController.java
+++ src/main/java/urfu/student/helper/controllers/StudentController.java
@@ -1,34 +1,16 @@
 package urfu.student.helper.controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import urfu.student.helper.models.student.Student;
-import urfu.student.helper.models.student.dto.StudentRegistryDTO;
-import urfu.student.helper.repositories.StudentRepository;
-import urfu.student.helper.services.StudentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import urfu.student.helper.services.student.StudentService;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
+@AllArgsConstructor
 @RestController
-@RequestMapping("/urfuhelper/students")
+@RequestMapping("/student")
 public class StudentController {
-    @Autowired
     private final StudentService studentService;
 
-    public StudentController(StudentService studentService){
-        this.studentService = studentService;
-    }
 
 }
