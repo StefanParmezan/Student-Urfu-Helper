c592ab40231877e918591ed08ea811e44adc21be
daniil4jk <daniilprotop@bk.ru>
2025-11-22 12:54:09 +0500

    Изменена сигнатура

--- src/main/java/urfu/student/helper/ai/AiService.java
+++ src/main/java/urfu/student/helper/ai/AiService.java
@@ -1,9 +1,10 @@
 package urfu.student.helper.ai;
 
 import reactor.core.publisher.Flux;
+import urfu.student.helper.db.chat.ChatEntity;
 import urfu.student.helper.db.student.StudentEntity;
 
 public interface AiService {
     Flux<String> callToNewChat(String userRequest, StudentEntity studentEntity);
-    Flux<String> callByChatId(String userRequest, Long chatId);
+    Flux<String> callByChatId(String userRequest, ChatEntity chat);
 }
