e53efc242cbabf069a86f67fd4cffdc0e098d76c
daniil4jk <daniilprotop@bk.ru>
2025-11-22 10:10:53 +0500

    Исправлена структура контроллеров

--- src/main/java/urfu/student/helper/controllers/ChatController.java
+++ src/main/java/urfu/student/helper/controllers/ChatController.java
@@ -6,11 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import urfu.student.helper.ai.AiService;
+import urfu.student.helper.models.student.Student;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
+    private final AiService aiService;
 
+    @PostMapping("/{chatId}")
+    public Flux<String> call(String message, Long chatId) {
+        return aiService.callByChatId(message, chatId);
+    }
 
+    @PostMapping("/new")
+    public Flux<String> call(String message) {
+        Student student; //TODO Получить всё таки студента
+        return aiService.callToNewChat(message, student);
+    }
 }
--- src/main/java/urfu/student/helper/controllers/MessageController.java
+++ src/main/java/urfu/student/helper/controllers/MessageController.java
@@ -11,11 +11,5 @@ import urfu.student.helper.ai.AiService;
 @RestController
 @RequestMapping("/chat/message")
 public class MessageController {
-    private final AiService aiService;
 
-    @PostMapping("/send")
-    public Flux<String> call(String message, Long chatId) {
-        return aiService.callByChatId(message, chatId);
-        //TODO реалоизовать вызов на новый чат
-    }
 }
