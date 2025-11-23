f97baf6545cb2d8a3ed880e2c3d9a5584b7455aa
daniil4jk <daniilprotop@bk.ru>
2025-11-23 10:44:40 +0500

    Драйверсетап

--- src/main/java/urfu/student/helper/security/parser/SeleniumParser.java
+++ src/main/java/urfu/student/helper/security/parser/SeleniumParser.java
@@ -1,9 +1,13 @@
 package urfu.student.helper.security.parser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class SeleniumParser implements AutoCloseable {
+    {
+        WebDriverManager.chromedriver().setup();
+    }
     @Getter
     private final ChromeDriver driver = new ChromeDriver();
 
