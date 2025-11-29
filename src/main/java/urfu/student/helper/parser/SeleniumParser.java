b14e33d8ad3e28177f8e82a2351c9c422671f5f9
StefanParmezan <syromatnikov26@gmail.com>
2025-11-29 15:10:15 +0500

    Убран @Component и @PostConstruct, всё вынесено в конструктор

--- src/main/java/urfu/student/helper/parser/SeleniumParser.java
+++ src/main/java/urfu/student/helper/parser/SeleniumParser.java
@@ -1,19 +1,15 @@
 package urfu.student.helper.parser;
 
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.stereotype.Component;
 
 @Getter
-@Component
 public class SeleniumParser implements AutoCloseable {
-    private WebDriver driver;
+    private final WebDriver driver;
 
-    @PostConstruct
-    public void init() {
+    public SeleniumParser() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
